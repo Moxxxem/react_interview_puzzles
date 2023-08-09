33e6169b0666a0badab8162a7e5ed1cfa9c15c0a
Moxxxem <m.metus@tg.dunice.net>
2023-08-09 12:02:42 +0300

    water_container

--- water_container/src/App.tsx
+++ water_container/src/App.tsx
@@ -1,33 +1,62 @@
+import { useEffect } from 'react';
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+enum Direction {
+  UP = 'up',
+  DOWN = "down"
+}
 
+type Values = 0 | 20 | 40 | 60 | 80 | 100
+type Status = "Набираем" | "Выливаем" | "Пусто" | "Набрано"
+
+function App() {
+  const [waterLevel, setWaterLevel] = useState<Values>(0);
+  const [status, setStatus] = useState<Status>("Пусто");
+  const [direction, setDirection] = useState<Direction | undefined>(undefined);
+
+  useEffect(() => {
+    const ableToFill = direction === Direction.UP && waterLevel < 100;
+    const ableToEmpty = direction === Direction.DOWN && waterLevel > 0;
+
+    const intId = setInterval(() => {
+      if (ableToFill) {
+        setWaterLevel(waterLevel => (waterLevel += 20) as Values)
+        setStatus("Набираем");
+      }
+      if (ableToEmpty) {
+        setWaterLevel(waterLevel => (waterLevel -= 20) as Values)
+        setStatus("Выливаем");
+      }
+    }, 1000)
+
+    if (waterLevel === 100) {
+      setStatus("Набрано");
+    } else if (waterLevel === 0) {
+      setStatus("Пусто");
+    }
+
+    return () => clearInterval(intId);
+
+  }, [direction, waterLevel])
+
+  function fillBucket() {
+    setDirection(Direction.UP)
+    // setInterval(() => setWaterLevel(waterLevel + 20), 1000);
+  }
+
+  function emptyBucket() {
+    setDirection(Direction.DOWN)
+    // setInterval(() => setWaterLevel(waterLevel - 20), 1000);
+  }
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>Status: {status}</p>
+        <div className='bucket' style={{ height: waterLevel }} />
+        <button onClick={fillBucket}>Набрать</button>
+        <button onClick={emptyBucket}>Опустошить</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
